7f6857393b4bbfc198c50d1b213ebe45b6e0ea39
F.Uhart <fabien.uhart@gmail.com>
2020-03-10 15:10:33 +0100

    mise a jour des evnets
    a modifier le readme

---
+++ DatetimeEventStore.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sqlite3
+
+
+class DatetimeEventStore:
+    def __init__(self):
+        """ Create Table if not exist, open the connection """
+        try:
+            self.conn = sqlite3.connect('EventsTable.db')
+            c = self.conn.cursor()
+            c.execute(
+                '''CREATE TABLE IF NOT EXISTS EVENTS ([generated_id] INTEGER PRIMARY KEY,[Name] text, [Date] date)''')
+            self.conn.commit()
+        except sqlite3.Error as e:
+            print(e)
+
+    def __del__(self):
+        self.conn.close()
+
+    def store_event(self, at, data):
+        try:
+            c = self.conn.cursor()
+            sqlite_insert_with_param = """INSERT INTO EVENTS(Name, Date) VALUES (?, ?);"""
+            data_tuple = (data, at)
+            c.execute(sqlite_insert_with_param, data_tuple)
+            self.conn.commit()
+        except sqlite3.Error as error:
+            print("Failed to insert", error)
+
+    def get_events(self, start, end):
+        strStart = start.strftime("%Y-%m-%d")
+        strEnd = end.strftime("%Y-%m-%d")
+        c = self.conn.cursor()
+        sqlite_events_between = """SELECT NAME, DATE from EVENTS WHERE Date BETWEEN ? and ?;"""
+        data_tuple = (strStart, strEnd)
+        c.execute(sqlite_events_between, data_tuple)
+        rows = c.fetchall()
+        self.conn.commit()
+        return rows
+
+    def truncate(self):
+        c = self.conn.cursor()
+        sqlite_truncate = '''DELETE FROM EVENTS;'''
+        c.execute(sqlite_truncate)
+        self.conn.commit()
